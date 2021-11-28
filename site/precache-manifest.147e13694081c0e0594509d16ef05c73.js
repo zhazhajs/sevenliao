6fb8cb585e6c9254a4bb918de20a41f79bb331cb
zhazhajs <jszhazha@126.com>
2021-11-28 18:24:14 +0800

    up

---
+++ site/precache-manifest.147e13694081c0e0594509d16ef05c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb8141015f383f2743ad2ea72131306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "072248bde9641fb03f77",
+    "url": "/static/css/main.57fd5c0f.chunk.css"
+  },
+  {
+    "revision": "b43bc60de3d08583578b",
+    "url": "/static/js/2.35fb9b71.chunk.js"
+  },
+  {
+    "revision": "ac4e94f3c34e52aecf7b",
+    "url": "/static/js/3.ea4f4ae5.chunk.js"
+  },
+  {
+    "revision": "072248bde9641fb03f77",
+    "url": "/static/js/main.03877be7.chunk.js"
+  },
+  {
+    "revision": "124578f7cc0b5b6a79cc",
+    "url": "/static/js/runtime~main.62c82995.js"
+  }
+]);
