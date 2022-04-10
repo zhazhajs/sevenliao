ea653489902aad46185d6ebedb272abc873f385a
zhazhajs <jszhazha@126.com>
2022-04-10 18:13:26 +0800

    update

---
+++ precache-manifest.db02411c360d96a2eeaddeaa0ac68790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de83d317f582f15b379143236653e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9f4347f2fab2c117a04",
+    "url": "/static/css/main.cc75e6dd.chunk.css"
+  },
+  {
+    "revision": "6e908996940378782e35",
+    "url": "/static/js/2.de5873d8.chunk.js"
+  },
+  {
+    "revision": "279d6af85f04614f3d15",
+    "url": "/static/js/3.dbebf3bf.chunk.js"
+  },
+  {
+    "revision": "c9f4347f2fab2c117a04",
+    "url": "/static/js/main.e35c24dc.chunk.js"
+  },
+  {
+    "revision": "9b5c3047a1ff7347a2a0",
+    "url": "/static/js/runtime~main.3b4f11cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91427ae5fc28cea8b042098c9ffc7cfd.js"
+  "/precache-manifest.db02411c360d96a2eeaddeaa0ac68790.js"
 );
 
 self.addEventListener('message', (event) => {
